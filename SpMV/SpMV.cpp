f18fbba01cbf520452d3f990f5c9b67eb00bb882
Konstantinos Sotiropoulos <sot.kosths@gmail.com>
2023-07-10 01:18:19 +0200

    [SpMV] Add second slicing

--- SpMV/SpMV.cpp
+++ SpMV/SpMV.cpp
@@ -3,11 +3,6 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
 
-
-// What kind of annotations shall the programmer provide?
-//
-// Option 1: Help the compiler "understand" if/where is the indirection
-//
 #pragma dpref indirection(Values[Offsets], X[ColIdx[Offsets]])
 void SpMV(const size_t N,
 	  const size_t * __restrict Offsets,
@@ -19,8 +14,8 @@ void SpMV(const size_t N,
     Y[I] = 0;
     for (size_t J = Offsets[I]; J < Offsets[I + 1]; J++) {
 
-      // Option 2: "Describe" a slice by annotating a scope with in/out params
-      #pragma dpref slice in(Values[J], ColIdx[J])
+      // Provide a "criterion" for the first slicing
+      #pragma dpref sliceOn(J)
       Y[I] += Values[J] * X[ColIdx[J]];
     }
   }
